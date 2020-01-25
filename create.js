c913180ae76e3e749b9eba529a6cbdff2e58473e
Hilda <hilda.kh.amiri@gmail.com>
2020-01-24 18:46:44 -0800

    function exporession style fixed

--- create.js
+++ create.js
@@ -3,7 +3,7 @@ import AWS from "aws-sdk";
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-export function main(event, context, callback) {
+export const main = function(event, context, callback) {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
@@ -52,4 +52,4 @@ export function main(event, context, callback) {
     };
     callback(null, response);
   });
-}
+};
