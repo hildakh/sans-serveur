bc09f5a12b713df21a7ae384fd7f6da825692852
Hilda <hilda.kh.amiri@gmail.com>
2020-01-27 17:05:26 -0800

    comments on the billing test json file added to billing function as json cannot contain comments

--- billing.js
+++ billing.js
@@ -2,6 +2,7 @@ import stripePackage from "stripe";
 import { calculateCost } from "./libs/billing-lib";
 import { success, failure } from "./libs/response-lib";
 
+// tok_visa (used in the billing test) is a stripe test token; the test uses 21 as the number of notes the user wants to store (in the billing test) >> billing test is a json and would not allow comments
 export const main = async function(event, context) {
   const {storage, source} = JSON.parse(event.body);
   const amount = calculateCost(storage);
